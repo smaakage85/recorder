f75e318de337fb98e78c82f0f5d7ecc616037b16
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2019-04-20 22:35:45 +0200

    Hex sticker

---
+++ man/figures/create_sticker.R
@@ -0,0 +1,8 @@
+library(hexSticker)
+
+sticker("man/figures/logo.png",
+        package = "recordr",
+        p_size = 14, p_color = "black", p_x = 1, p_y = 1.35,
+        s_x = 1, s_y = 0.9,
+        h_color = "black", h_fill = "white",
+        filename = "man/figures/logo_hex.png")
