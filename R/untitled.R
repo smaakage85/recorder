618dfc984dc44f4cfa46b65e733f29db1236a860
Lars Kjeldgaard <lars.kjeldgaard@ufst.dk>
2019-04-26 13:01:17 +0200

    Matrix experiments.

--- R/untitled.R
+++ R/untitled.R
@@ -4,3 +4,14 @@
 
 # playb$detailed_checks$new_level
 
+record(df) -> tape
+
+play(newdata, tape) -> playback
+
+dc <- playback$detailed_checks[c("outside_range", "new_level", "new_NA")]
+library(data.table)
+check_matrix <- function(x) {
+  dts <- lapply(dc, data.table::as.data.table)
+  m <- do.call(cbind, dts)
+}
+dd <- check_matrix(dc)
