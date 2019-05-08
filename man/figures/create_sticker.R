7f828b0e47077a99e8d555af66a438d345ff6c91
Lars Kjeldgaard <lars.kjeldgaard@ufst.dk>
2019-05-08 16:38:49 +0200

    new logo fixes

--- man/figures/create_sticker.R
+++ man/figures/create_sticker.R
@@ -1,9 +1,9 @@
 library(hexSticker)
 
-sticker("man/figures/logo.png",
+sticker("man/figures/logo_recordr.png",
         package = "recordr",
-        p_size = 14, p_color = "black", p_x = 1, p_y = 1.35,
-        s_x = 1, s_y = 0.9,
+        p_size = 20, p_color = "black", p_x = 1, p_y = 1.35,
+        s_x = 1, s_y = 0.75, s_width = 0.55,
         h_color = "black", h_fill = "white",
         filename = "man/figures/logo_hex.png")
 
